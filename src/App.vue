4921307c96995aef0b8c0627c433d6ca9af16966
Stoner609 <Stoner7969@gmail.com>
2017-05-17 16:41:47 +0800

    modify

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <br>
     {{eat}}
     <br>
-    <button class="vue-btn" @click='GO'>選我</button>
+    <button class="vue-btn" @click='RandomEat'>選我</button>
   </div>
 </template>
 
@@ -14,14 +14,31 @@ export default {
     return {
       msg: 'Welcome to Xihu',
       eat: '吃什小',
-      eats: ['早餐店', '阿婆水餃', '韓式', '小巷子', '海霸王', '麵線']
+      eats: [ 
+        '早餐店', 
+        '阿婆水餃', 
+        '韓式', 
+        '小巷子', 
+        '海霸王', 
+        '很遠麵線', 
+        '鬆餅', 
+        '湯餃',
+        '麥當勞',
+        '肯德基',
+        '甜不辣',
+        '火雞肉飯'
+      ]
     }
   },
   methods: {
-    GO() {
+    RandomEat() {
+      let previous_eat = this.eat
       let random_id = Math.floor(Math.random() * this.eats.length)
-      console.log(random_id)
-      this.eat = this.eats[random_id]
+      if (this.eats[random_id] === previous_eat) {
+        this.RandomEat()
+      }else {
+        this.eat = this.eats[random_id]
+      }
     }
   }
 }
