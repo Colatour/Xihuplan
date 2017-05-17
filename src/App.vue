723aaaf9b39b8db1b8deada9e449eeac018db72e
Stoner609 <Stoner7969@gmail.com>
2017-05-17 18:34:34 +0800

    Modify

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
   <div id="app">
-    {{msg}}
-    <br>
-    {{eat}}
-    <br>
+    中午要吃什麼？
+    <select>
+      <option 
+        v-for='(item, index) in locations'
+        :value=item.value
+        key='index'
+      >
+        {{item.name}}
+      </option>
+    </select>
     <button class="vue-btn" @click='RandomEat'>選我</button>
+    <br>
+    <div class="col-md-4">
+      <div class="character-card">
+        <div class="card-block">
+          <h4 class="card-title">{{eat.name}}</h4>
+          <p class="card-text">平均價錢： {{eat.price}}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,26 +27,31 @@
 export default {
   data () {
     return {
-      msg: 'Welcome to Xihu',
-      eat: '吃什小',
+      selected: {name: '西湖', value: 'xihu'},
+      locations: [ 
+        {name: '西湖', value: 'xihu'}, 
+        {name: '台北車站', value: 'taipeistation'} 
+      ],
+      eat: '',
       eats: [ 
-        '早餐店', 
-        '阿婆水餃', 
-        '韓式', 
-        '小巷子', 
-        '海霸王', 
-        '很遠麵線', 
-        '鬆餅', 
-        '湯餃',
-        '麥當勞',
-        '肯德基',
-        '甜不辣',
-        '火雞肉飯'
+        {name: '早餐店', price: 40, location: '西湖'}, 
+        {name: '阿婆水餃', price: 70, location: '西湖'},
+        {name: '有禮貌韓式', price: 70, location: '西湖'},
+        {name: '小巷子', price: 80-100, location: '西湖'},
+        {name: '海霸王炒飯', price: 70, location: '西湖'},
+        {name: '很遠麵線', price: 30, location: '西湖'},
+        {name: '牛肉湯餃', price: 80, location: '西湖'},
+        {name: '麥當勞', price: 100, location: '西湖'},
+        {name: '肯德基', price: 100, location: '西湖'},
+        {name: '香菜甜不辣', price: 40, location: '西湖'},
+        {name: '火車便當', price: 60, location: '西湖'},
+        {name: '吵架花枝羹', price: 60, location: '西湖'}
       ]
     }
   },
   methods: {
     RandomEat() {
+      let array = this.eats.filter(this.isValue)
       let previous_eat = this.eat
       let random_id = Math.floor(Math.random() * this.eats.length)
       if (this.eats[random_id] === previous_eat) {
@@ -39,7 +59,13 @@ export default {
       }else {
         this.eat = this.eats[random_id]
       }
+    },
+    isValue() {
+      console.log();
     }
+  },
+  created() {
+    this.RandomEat()
   }
 }
 </script>
