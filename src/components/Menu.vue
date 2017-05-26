1edaa8f33d175b024b96a09c0dc2ca3739916b91
Stoner609 <Stoner7969@gmail.com>
2017-05-26 08:54:12 +0800

    test jsonbin

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -28,7 +28,7 @@
             // local端先用Chrome抓取一個跨域小套件
             // https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi
             GO: function() {
-                fetch(`http://opendata.epa.gov.tw/ws/Data/REWXQA/?%24orderby=SiteName&%24skip=0&%24top=1000&format=json`
+                fetch(`https://jsonbin.org/stoner609/Airquality/`
                  //,{method: 'GET', mode: 'no-cors'}
                  //,{method: 'GET', mode: 'cors'}
                  ,{method: 'GET'}
